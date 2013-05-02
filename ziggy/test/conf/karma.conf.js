12449e96505a6968238026527fe4b2d68bbc02bf
Kiran <kirang20@gmail.com>
2013-05-02 14:51:10 +0530

    Kiran | Added travis ci configurations. Removed redundant libs. Modified karma.conf.js to reflect new directory structure.

--- ziggy/test/conf/karma.conf.js
+++ ziggy/test/conf/karma.conf.js
@@ -1,16 +1,9 @@
-basePath = '../../../../';
+basePath = '../../../';
 
 files = [
     JASMINE,
     JASMINE_ADAPTER,
-    'assets/js/jquery.min.js',
-    'assets/js/lib/*.js',
-    'assets/js/page_view.js',
-    'assets/js/anmNavigation.js',
-    'assets/js/form/*.js',
-    'assets/js/smart_registry/app.js',
-    'assets/js/smart_registry/*.js',
-    'src/test/js/lib/*.js',
-    'src/test/js/*spec.js',
-    'src/test/js/smart_registry/*spec.js'
+    'ziggy/src/*.js',
+    'ziggy/test/lib/*.js',
+    'ziggy/test/*spec.js'
 ];
